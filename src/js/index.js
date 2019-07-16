b379e85e16ec3d21ba7a0c2ffa6cd355ea350b61
justynamak <justyna.zdunkowska@gmail.com>
2019-07-16 20:42:35 +0200

    dodano nasluchiwanie na zdarzenieresize

--- src/js/index.js
+++ src/js/index.js
@@ -16,4 +16,4 @@ const config = {
 };
 const loadedProducts = getProducts(config);
 mainCart.handleToggleContent();
-window.addEventListener("resize", () => getProducts(config), { once: true });
+window.addEventListener("resize", () => paginator.createPagination());
