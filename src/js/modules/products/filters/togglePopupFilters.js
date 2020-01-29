ce9bfb7a6e6b6d36d4e98343e19a6330a3dfddeb
justynaMak <justynamakuch@outlook.com>
2020-01-29 22:41:16 +0100

    obsługa zamykania popupu po kliknięciu na przycisk

--- src/js/modules/products/filters/togglePopupFilters.js
+++ src/js/modules/products/filters/togglePopupFilters.js
@@ -2,6 +2,7 @@ export default function(e) {
   const parent = e.currentTarget.parentNode;
   const popup = parent.querySelector(".popup");
   const close = parent.querySelector(".popup__close");
+  const filterButton = parent.querySelector(".filter__button");
 
   const closePopup = () => {
     close.classList.remove("active");
@@ -14,4 +15,8 @@ export default function(e) {
   openPopup();
   close.addEventListener("click", closePopup);
   window.addEventListener("resize", closePopup);
+
+  if (filterButton) {
+    filterButton.addEventListener("click", closePopup);
+  }
 }
